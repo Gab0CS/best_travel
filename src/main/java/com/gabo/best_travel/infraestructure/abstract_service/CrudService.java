7f29a0acc31b5a36b6e5fc7c88f0b0f443d1cf4f
Gab0CS <gabocanseco@gmail.com>
2025-02-21 20:21:59 -0600

    Crud service correction

--- src/main/java/com/gabo/best_travel/infraestructure/abstract_service/CrudService.java
+++ src/main/java/com/gabo/best_travel/infraestructure/abstract_service/CrudService.java
@@ -1,19 +1,8 @@
 package com.gabo.best_travel.infraestructure.abstract_service;
 
 public interface CrudService <RQ, RS, ID>{
-    RS create(RQ request){
-
-    }
-
-    RS read(ID id){
-
-    }
-
-    RS update(RQ request, ID id){
-
-    }
-
-    void delete(ID id){
-
-    }
+    RS create(RQ request);
+    RS read(ID id);
+    RS update(RQ request, ID id);
+    void delete(ID id);
 }
